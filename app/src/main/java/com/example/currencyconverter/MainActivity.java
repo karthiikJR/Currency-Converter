b5c8a848fd396a94e22aa0d9f3bdae67f07d2c81
KarthikJR <karthikjrao69@gmail.com>
2023-03-21 17:46:30 +0530

    master

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void currClick(View view) {
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        /* adding elements to spinner
+        1. Create a spinner obj
+        2. Create a array list
+        3. Add required elements
+        4. We use ArrayAdapter to attach the ArrayList
+        5. Add it to spinner
+        */
+
+        Spinner spinner = findViewById(R.id.spinnerCurrList);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Dollar");
+        arrayList.add("Pound");
+        arrayList.add("Yen");
+        arrayList.add("Euro");
+        arrayList.add("Riyal");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arrayList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
     }
 }
